537d24f417cc305631ec1b57ef58f853a76adcb6
jonathandennis <4jondennis@gmail.com>
2020-06-08 14:05:15 +0200

    unicafe lesson notes

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,28 +1,4 @@
 //////////////////////////////////////////////////////////////////
-//////   EXERCISE 1.6: unicafe step1
-//////////////////////////////////////////////////////////////////
-
-import ReactDOM from 'react-dom'
-
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  return (
-    <div>
-      code here
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
-
-
-//////////////////////////////////////////////////////////////////
 //////   LESSON 1d LECTURE NOTES A more complex state, debugging React apps 
 //////////////////////////////////////////////////////////////////
 

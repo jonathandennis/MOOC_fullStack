310111ae3d5aa67414dc2e020a1b98b0bd921218
jonathandennis <4jondennis@gmail.com>
2020-06-09 14:14:22 +0200

    1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,7 +1,146 @@
-//////////////////////////////////////////////////////////////////
-//////   EXERCISE 1.7: unicafe step2
-//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+//////   EXERCISE 1.8: unicafe step3
+/////////////////////////////////////////////////////
 
+
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const Good = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+       {props.text} {props.good}
+      </p>
+    </div>
+  )
+}
+
+const Neutral = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+       {props.text} {props.neutral}
+      </p>
+    </div>
+  )
+}
+
+const Bad = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+       {props.text} {props.bad}
+      </p>
+    </div>
+  )
+}
+
+const All = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+       {props.text} {props.all}
+      </p>
+    </div>
+  )
+}
+
+const ScoreAvg = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+       {props.text} {props.scoreAvg}
+      </p>
+    </div>
+  )
+}
+
+const PositiveAvg = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+       {props.text} {props.positiveAvg}
+      </p>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <Good good={props.good} text="good:" />
+      <Neutral neutral={props.neutral} text="neutral:" />
+      <Bad bad={props.bad} text="bad:" />
+      <All all={props.all} text="all:" />
+      <ScoreAvg scoreAvg={props.scoreAvg} text="average:" />
+      <PositiveAvg positiveAvg={props.positiveAvg} text="positive:" />
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad;
+  const scoreAvg = ((good * 1) + (neutral * 0) + (bad * -1)) / all;
+  const positiveAvg = (good * 100) / all;
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }  
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)  
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <Statistics good={good}      
+                  neutral={neutral}
+                  bad={bad}
+                  all={all}
+                  scoreAvg={scoreAvg}
+                  positiveAvg={positiveAvg}
+                   />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
+
+
+/////////////////////////////////////////////////////
+//////   EXERCISE 1.7: unicafe step2
+/////////////////////////////////////////////////////
+
+/* 
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
@@ -52,11 +191,11 @@ const App = () => {
 ReactDOM.render(<App />, 
   document.getElementById('root')
 )
+  */
 
-
-//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 //////   EXERCISE 1.6: unicafe step1
-//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 /* 
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'

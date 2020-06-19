cf67b2078d960c1acadcfcc22521de462b885d05
jonathandennis <4jondennis@gmail.com>
2020-06-19 09:37:19 +0200

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,3 +1,62 @@
+//////////////////////////////////////////////////
+//////   2.7: The Phonebook Step2
+//////////////////////////////////////////////////
+
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+const App = (props) => {
+  const [ persons, setPersons ] = useState(props.persons) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+    persons.some(person => person.name === newName) ?
+    window.alert(`${newName} is already added to phonebook`) :
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+                  value={newName}
+                  onChange={handleNameChange} 
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={{ padding: 0 }}>
+        {persons.map(person => 
+          <Person key={person.id} person={person} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
+
+//////////////////////////////////////////////////
+//////   2.6: The Phonebook Step1
+//////////////////////////////////////////////////
+/* 
 import React, { useState } from 'react'
 import Person from './components/Person'
 
@@ -46,4 +105,5 @@ const App = (props) => {
   )
 }
 
-export default App
+export default App 
+*/
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,3 +1,35 @@
+//////////////////////////////////////////////////
+//////   2.7: The Phonebook Step2
+//////////////////////////////////////////////////
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace'
+  },
+]
+
+//console.log(persons.includes('Ada Lovelace'))
+
+ReactDOM.render(
+  <App persons={persons} />,
+  document.getElementById('root')
+);
+
+
+//////////////////////////////////////////////////
+//////   2.6: The Phonebook Step1
+//////////////////////////////////////////////////
+
+/* 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -16,4 +48,5 @@ const persons = [
 ReactDOM.render(
   <App persons={persons} />,
   document.getElementById('root')
-);
+); 
+*/

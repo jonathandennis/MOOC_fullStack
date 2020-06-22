5e972700d633c2f88eff7ada77afae0cc74136a8
jonathandennis <4jondennis@gmail.com>
2020-06-22 17:00:34 +0200

    2.9: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,103 @@
+
+//////////////////////////////////////////////////
+//////   2.9: The Phonebook Step4
+//////////////////////////////////////////////////
+
+import React, { useState } from 'react'
+
+const App = (props) => {
+  const [ persons, setPersons ] = useState(props.persons)
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456' },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523' },
+  //   { name: 'Dan Abramov', number: '12-43-234345' },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  // ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ searchTerm, setSearchTerm ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    }
+    persons.some(person => person.name === newName) ?
+    window.alert(`${newName} is already added to phonebook`) :
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+  
+  const results = !searchTerm
+  ? persons
+  : persons.filter(person => 
+      person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+  
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setSearchTerm(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+        <p>filter shown with <input
+                            type="text"
+                            placeholder="Search"
+                            value={searchTerm}
+                            onChange={handleFilterChange} 
+                          />
+        </p>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+                  value={newName}
+                  onChange={handleNameChange} 
+                />
+        </div>
+        <div>
+          number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange} 
+                  />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style={{ padding: 0 }}>
+        {results.map(person => (
+          <li key={person.id} style={{ listStyleType: "none" }}>{person.name} {person.number}</li>
+        )
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
+
 //////////////////////////////////////////////////
 //////   2.8: The Phonebook Step3
 //////////////////////////////////////////////////
-
+/* 
 import React, { useState } from 'react'
 import Person from './components/Person'
 
@@ -64,7 +160,7 @@ const App = (props) => {
 }
 
 export default App
-
+*/
 
 //////////////////////////////////////////////////
 //////   2.7: The Phonebook Step2

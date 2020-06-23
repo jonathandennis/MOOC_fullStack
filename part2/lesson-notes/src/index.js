e13c3fc11a3c55a0a775b04095f4d4ccfa64fa18
jonathandennis <4jondennis@gmail.com>
2020-06-23 22:47:35 +0200

    part 2 lesson notes

--- part2/lesson-notes/src/index.js
+++ part2/lesson-notes/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 const notes = [
   {
@@ -29,4 +29,4 @@ const notes = [
 ReactDOM.render(
   <App notes={notes} />,
   document.getElementById('root')
-)
+);

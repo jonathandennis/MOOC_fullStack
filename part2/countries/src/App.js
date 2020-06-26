d5359086a65fc9cd390e1c76f5cef7e667874f94
jonathandennis <4jondennis@gmail.com>
2020-06-26 10:23:16 +0200

    2.13 Data for countries, step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,55 @@
 //////////////////////////////////////////////////
+//////   2.13 Data for countries, step2
+//////////////////////////////////////////////////
+
+
+import React, { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import Countries from './components/Countries'
+
+import axios from 'axios'
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [ searchTerm, setSearchTerm ] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+  console.log('Countries', countries)
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setSearchTerm(event.target.value)
+  }
+  
+  return (
+    <div>
+      <Filter searchTerm={searchTerm}
+              handleFilterChange={handleFilterChange} 
+      />
+      <Countries countries={countries}
+                 searchTerm={searchTerm}
+                 setSearchTerm={setSearchTerm} 
+      />
+    </div>
+  );
+}
+
+export default App;
+
+
+//////////////////////////////////////////////////
 //////   2.12 Data for countries, step1
 //////////////////////////////////////////////////
 
+/* 
 import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Countries from './components/Countries'
@@ -42,3 +90,4 @@ const App = () => {
 
 export default App;
 
+*/
--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,7 +1,66 @@
 //////////////////////////////////////////////////
-//////   2.12 Data for countries, step1
+//////   2.13 Data for countries, step2
 //////////////////////////////////////////////////
 
+
+import React from 'react'
+
+const Countries = ({ countries, searchTerm, setSearchTerm }) => {
+    
+    const countriesToShow = !searchTerm
+        ? countries
+        :countries.filter(country => 
+            country.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+
+        if (countriesToShow.length === 1) {
+            //console.log('countriesToShow[0]:', countriesToShow[0])
+            return (
+                <div>
+                    <h1>{countriesToShow[0].name}</h1>
+                    <p>capital {countriesToShow[0].capital}</p>
+                    <p>population {countriesToShow[0].population}</p>
+                    <h3>languages</h3>
+                    <ul>
+                        {countriesToShow[0].languages.map(language => (
+                            <li key={language.name}>{language.name}</li>
+                        ))} 
+                    </ul>
+                    <img width="100" height="100" src={countriesToShow[0].flag} alt={countriesToShow[0].name}/> 
+                </div>
+            )
+        }
+
+        if (countriesToShow.length > 1 && countriesToShow.length < 10) {
+            
+            return (
+                <div>
+                    <ul style={{ padding: 0 }}>
+                        {countriesToShow.map(country => (
+                            <li onClick={() => setSearchTerm(country.name)} key={country.numericCode} style={{ listStyleType: "none" }} >{country.name} <button type="submit">show</button></li>
+                        )
+                        )}
+                    </ul>
+                </div>
+            )
+        }
+
+        if (searchTerm === '') {
+            return ('')
+        }
+     
+        return (
+            <p>Too many matches, specify another filter</p>
+        )
+}
+
+export default Countries
+
+
+//////////////////////////////////////////////////
+//////   2.12 Data for countries, step1
+//////////////////////////////////////////////////
+/* 
 import React from 'react'
 
 const Countries = ({ countries, searchTerm }) => {
@@ -52,3 +111,5 @@ const Countries = ({ countries, searchTerm }) => {
 }
 
 export default Countries
+
+*/

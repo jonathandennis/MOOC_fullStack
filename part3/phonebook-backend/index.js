0736dd7f7cce8dae8c7bff3107380bbb6dcf6024
jonathandennis <4jondennis@gmail.com>
2020-07-06 15:15:26 +0200

    3.2: Phonebook backend step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,58 @@
 //////////////////////////////////////////////////
-//////   3.1: Phonebook backend step1
+//////   3.2: Phonebook backend step2
 //////////////////////////////////////////////////
 
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 2
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 3
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 4
+    }
+  ]
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Phonebook Backend</h1>')
+  })
+
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+  app.get('/info', (req, res) => {
+      const numPersons = persons.length
+      res.send(`<p>Phonebook has info for ${numPersons} people</p>
+      ${new Date()}`)
+      
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
+
+//////////////////////////////////////////////////
+//////   3.1: Phonebook backend step1
+//////////////////////////////////////////////////
+/* 
 const express = require('express')
 const app = express()
 
@@ -37,8 +88,10 @@ let persons = [
   app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
+  
 
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+  }) 
+  */

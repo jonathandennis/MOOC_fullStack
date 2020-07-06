d004f9111955f26446f9d986ce014892b4296e69
jonathandennis <4jondennis@gmail.com>
2020-07-06 16:27:58 +0200

    3.5: Phonebook backend step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,86 @@
 //////////////////////////////////////////////////
-//////   3.4: Phonebook backend step4
+//////   3.5: Phonebook backend step5
 //////////////////////////////////////////////////
 
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 2
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 3
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 4
+    }
+  ]
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Phonebook Backend</h1>')
+  })
+
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+  app.get('/info', (req, res) => {
+    const numPersons = persons.length
+    res.send(`<p>Phonebook has info for ${numPersons} people</p>
+    ${new Date()}`)    
+  })
+
+  app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      } 
+  })
+
+  app.post('/api/persons', (req, res) => {
+    const newId = Math.floor(Math.random() * 10000)
+    const person = req.body
+    person.id = newId
+
+    persons = persons.concat(person)
+  
+    res.json(person)
+  })
+
+  app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+  
+    res.status(204).end()
+  })
+
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+
+
+//////////////////////////////////////////////////
+//////   3.4: Phonebook backend step4
+//////////////////////////////////////////////////
+/* 
 const express = require('express')
 const app = express()
 
@@ -63,7 +142,7 @@ let persons = [
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
-
+ */
 
 //////////////////////////////////////////////////
 //////   3.3: Phonebook backend step3

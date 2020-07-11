c5ef021bc16e70830ed56eaaba64e1c50c38846e
jonathandennis <4jondennis@gmail.com>
2020-07-11 12:27:34 +0200

    part3 lesson-notes

---
+++ part3/lesson-notes/mongo.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.kk7vy.mongodb.net/note-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true,
+// })
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+
+///////////////////////////////////////
+////////// from connect mongo
+///////////////////////////////////////
+
+/* 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://fullstack:<password>@cluster0.kk7vy.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true
+*/

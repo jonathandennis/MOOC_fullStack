f2517343e2503113ef309058e1447488b6ec8eb9
jonathandennis <4jondennis@gmail.com>
2020-07-15 20:37:13 +0200

    3-5 lesson notes

--- part3/lesson-notes/index.js
+++ part3/lesson-notes/index.js
@@ -15,27 +15,6 @@ app.use(express.static('build'))
 app.use(cors())
 
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
-
   const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -65,14 +44,8 @@ let notes = [
     .catch(error => next(error))
   })
   
-  app.post('/api/notes', (request, response) => {
+  app.post('/api/notes', (request, response, next) => {
     const body = request.body
-  
-    if (body.content === undefined) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
   
     const note = new Note({
       content: body.content,
@@ -80,9 +53,11 @@ let notes = [
       date: new Date(),
     })
   
-    note.save().then(savedNote => {
-      response.json(savedNote)
+    note.save()
+      .then(savedNote => {
+        response.json(savedNote)
     })
+    .catch(error => next(error))
   })
 
   app.delete('/api/notes/:id', (request, response, next) => {
@@ -119,7 +94,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }

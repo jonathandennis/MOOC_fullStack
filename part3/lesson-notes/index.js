3ce6cda2aaca2f0c60168af89f3b3ca922818ca1
jonathandennis <4jondennis@gmail.com>
2020-07-21 14:39:21 +0200

    update lesson notes 3-6

--- part3/lesson-notes/index.js
+++ part3/lesson-notes/index.js
@@ -15,73 +15,72 @@ app.use(express.static('build'))
 app.use(cors())
 
 
-  const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  //console.log('request.params.id:', request.params.id)
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note){
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
   }
-  app.use(requestLogger)
- 
-
-  app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
-  })
-  
-  app.get('/api/notes/:id', (request, response, next) => {
-    //console.log('request.params.id:', request.params.id)
-    Note.findById(request.params.id)
-      .then(note => {
-        if (note){
-          response.json(note)
-        } else {
-          response.status(404).end()
-        }
+  console.log('request.params.id:', request.params.id)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
     })
     .catch(error => next(error))
-  })
-  
-  app.post('/api/notes', (request, response, next) => {
-    const body = request.body
-  
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-    })
-  
-    note.save()
-      .then(savedNote => {
-        response.json(savedNote)
-    })
-    .catch(error => next(error))
-  })
-
-  app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
-
-  app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
-  
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-    console.log('request.params.id:', request.params.id)
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-      .then(updatedNote => {
-        response.json(updatedNote)
-      })
-      .catch(error => next(error))
-  })
+})
 
 const unknownEndpoint = (request, response) => {
 response.status(404).send({ error: 'unknown endpoint' })
@@ -102,7 +101,8 @@ const errorHandler = (error, request, response, next) => {
 }
 
 app.use(errorHandler)
-  
+
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

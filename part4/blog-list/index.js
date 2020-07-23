90728e9541dd3320b0cdfe0ebd1635e4e026ff9d
jonathandennis <4jondennis@gmail.com>
2020-07-23 21:04:06 +0200

    finish refactor with app+index

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,41 +1,9 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const Blog = require('./models/blog')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
+const app = require('./app')
 const config = require('./utils/config')
-
-const cors = require('cors')
+const logger = require('./utils/logger')
 
 const server = http.createServer(app)
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.get('/api/blogs', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-    .catch(error => next(error))
-})
-
-app.post('/api/blogs', (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
-})
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -1,18 +1,6 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-const logger = require('../utils/logger')
 
-logger.info('connecting to', config.MONGODB_URI)
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.info('error connecting to MongoDB:', error.message)
-  })
-
-  const blogSchema = mongoose.Schema({
+const blogSchema = mongoose.Schema({
     title: String,
     author: String,
     url: String,

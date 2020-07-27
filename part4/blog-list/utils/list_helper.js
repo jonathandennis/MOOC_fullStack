b27c5df5ad800db706b96a461c03c0110853ff47
jonathandennis <4jondennis@gmail.com>
2020-07-27 11:40:33 +0200

    4.5*: helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,11 +1,25 @@
 const dummy = (blogs) => {
-  const reducer = (sum, item) => {
-    return sum + item
-  }
-
   return blogs.length === 0
     ? 1
-    : blogs.reduce(reducer, 0) / blogs.length
+    : 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  const getLikes = (sum, item) => {
+    return sum + item.likes
+  }
+  return blogs.reduce(getLikes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const getFavorite = (max, item) => {
+    console.log('Comparing: ', max, item)
+    return (max.likes > item.likes)
+      ? max
+      : item
+  }
+  const { author, title, likes } = blogs.reduce(getFavorite, 0)
+  return { author, title, likes }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }

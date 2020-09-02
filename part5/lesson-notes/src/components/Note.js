ed2f8fe74a71ebf14c0dad053ccddbfac77655e4
jonathandennis <4jondennis@gmail.com>
2020-09-02 15:05:20 +0200

    lesson-notes 5-8 -testing react apps

--- part5/lesson-notes/src/components/Note.js
+++ part5/lesson-notes/src/components/Note.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Note = ({ note, toggleImportance }) => {
   const label = note.important
-    ? 'make not important' : 'make important'
+    ? 'make not important'
+    : 'make important'
 
-  console.log('note.content in Note.js:', note.content)
+  //console.log('note.content in Note.js:', note.content)
 
   return (
     <li className='note'>

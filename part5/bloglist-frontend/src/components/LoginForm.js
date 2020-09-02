46e76abfe305367d3e2104b7ebb75c119c711cba
jonathandennis <4jondennis@gmail.com>
2020-09-02 10:01:15 +0200

    5.11: Blog list frontend, step11 -add proptypes

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
     handleSubmit,
@@ -36,5 +37,12 @@ const LoginForm = ({
     )
 }
 
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
 
-  export default LoginForm
+export default LoginForm

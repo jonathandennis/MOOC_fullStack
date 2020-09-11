76d0d3f89a84cd321deca6b0bc63c79353995601
jonathandennis <4jondennis@gmail.com>
2020-09-11 15:11:53 +0200

    part6 lesson-notes

---
+++ part6/lesson-counter/src/App.js
@@ -0,0 +1,49 @@
+
+//////////////////////////////////////////////////////////////
+////// My quick setup
+//////////////////////////////////////////////////////////////
+
+/* import React, { useState } from 'react';
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />     
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />           
+    </div>
+  )
+}
+
+export default App;
+ */

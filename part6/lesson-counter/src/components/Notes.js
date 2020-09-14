3f1a6633367a086b3856bea68d822cdd5063d895
jonathandennis <4jondennis@gmail.com>
2020-09-14 11:59:01 +0200

    Completed lesson 6b

--- part6/lesson-counter/src/components/Notes.js
+++ part6/lesson-counter/src/components/Notes.js
@@ -15,7 +15,14 @@ const Notes = () => {
     /* using hooks to dispatch actions. The useDispatch-hook provides any React component access to the dispatch-function of the redux-store defined in index.js. This allows all components to make changes to the state of the redux-store. */
   const dispatch = useDispatch()
   /* The component can access the notes stored in the store with the useSelector-hook of the react-redux library. useSelector receives a function as a parameter. The function either searches for or selects data from the redux-store. Here we need all of the notes, so our selector function returns the whole state: */
-  const notes = useSelector(state => state)
+  const notes = useSelector(({ filter, notes }) => {
+    if ( filter === 'ALL' ) {
+      return notes
+    }
+    return filter === 'IMPORTANT'
+    ? notes.filter(note => note.important)
+    : notes.filter(note => !note.important)
+  })
 
   return(
     <ul>
--- part6/lesson-counter/src/reducers/noteReducer.js
+++ part6/lesson-counter/src/reducers/noteReducer.js
@@ -1,5 +1,18 @@
+const initialState = [
+    {
+      content: 'reducer defines how redux store works',
+      important: true,
+      id: 1,
+    },
+    {
+      content: 'state of store can contain any data',
+      important: false,
+      id: 2,
+    },
+  ]
 
-const noteReducer = (state = [], action) => {
+const noteReducer = (state = initialState, action) => {
+  console.log('ACTION: ', action)
     switch(action.type) {
       case 'NEW_NOTE':
         return [...state, action.data]
@@ -17,27 +30,27 @@ const noteReducer = (state = [], action) => {
       default:
         return state
     }
-  }
+}
 
-  const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
+const generateId = () =>
+Number((Math.random() * 1000000).toFixed(0))
 
-  export const createNote = (content) => {
-    return {
-      type: 'NEW_NOTE',
-      data: {
-        content,
-        important: false,
-        id: generateId()
-      }
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
     }
   }
+}
     
-  export const toggleImportanceOf = (id) => {
-    return {
-      type: 'TOGGLE_IMPORTANCE',
-      data: { id }
-    }
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id }
   }
+}
 
-  export default noteReducer
+export default noteReducer

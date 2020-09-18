94f0236cece9360009e39c60c0c508afb9f194d6
jonathandennis <4jondennis@gmail.com>
2020-09-18 10:30:36 +0200

    Finished first half of lesson 6c

--- part6/lesson-notes/src/components/NewNote.js
+++ part6/lesson-notes/src/components/NewNote.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createNote } from '../reducers/noteReducer'
+import noteService from '../services/notes'
 
 const NewNote = (props) => {
     /* using hooks to dispatch actions. The useDispatch-hook provides any React component access to the dispatch-function of the redux-store defined in index.js. This allows all components to make changes to the state of the redux-store. */
   const dispatch = useDispatch()
 
-  const addNote = (event) => {
+  const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    dispatch(createNote(content))
+    const newNote = await noteService.createNew(content)
+    dispatch(createNote(newNote))
   }
 
   return (

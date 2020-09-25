56332a615bee20979aa87319fb69dcb2c3dc78db
jonathandennis <4jondennis@gmail.com>
2020-09-25 16:26:47 +0200

    6.21 anecdotes, the grand finale - fix vote bug, different solution without having to use useEffect and custom hook, lighter better

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,48 +1,75 @@
-import React from "react"
-import {useSelector} from "react-redux"
-import {useDispatch} from "react-redux"
-
-import {clearNotification} from "../reducers/notificationReducer"
+import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Notification = () => {
-  const dispatch = useDispatch()
+  const notification = useSelector(state => state.notification)
 
-  const {notification} = useSelector((s) => s)
+  console.log('notification: ', notification)
 
-  useClearNotificationHook(notification, dispatch, 3)
-  /* Custom Hook for manging dispatch call for clearing message. */
-  /* Signature => notificationMessage, dispatch, timeInSeconds */
   if (notification === null) {
     return null
   }
 
   const style = {
-    border: "solid",
+    border: 'solid',
     padding: 10,
     borderWidth: 1
-  };
-  return <div style={style}>{notification.message}</div>
-} /* End of Notification Component here. */
+  }
+  return (
+    <div style={style}>
+      {notification.message}
+    </div>
+  )
+}
 
 export default Notification
 
-const useClearNotificationHook = (notificationMessage, dispatch, timeInSeconds) => {
-  console.log = () => {} /* COMMENT this line to activate all the console.logs inside this hook. */
-  let timeIdRef = React.useRef()
-  React.useEffect(() => {
-    if (notificationMessage) {
-      timeIdRef.current = setTimeout(() => {
-        dispatch(clearNotification())
-        /* LOG1 */ console.log("::useeffect::DISPATCH successful for clearing notification.")
-        timeIdRef.current = null /* This will prevent un-necessary execution of clearUp function. */
-      }, timeInSeconds * 1000)
-      /* LOG2 */ console.log("::useeffect::SCHEDULED async dispatch call for id => ", timeIdRef.current)
-      return () => {
-        if (timeIdRef.current) {
-          clearTimeout(timeIdRef.current)
-          /* LOG3 */ console.log("::cleanUpFunction:: CLEARED async dispatch call for id => ", timeIdRef.current)
-        }
-      }
-    }
-  }, [notificationMessage, dispatch, timeInSeconds])
-}
+
+// import React from "react"
+// import {useSelector} from "react-redux"
+// import {useDispatch} from "react-redux"
+
+// import {clearNotification} from "../reducers/notificationReducer"
+
+// const Notification = () => {
+//   const dispatch = useDispatch()
+
+//   const {notification} = useSelector((s) => s)
+
+//   useClearNotificationHook(notification, dispatch, 3)
+//   /* Custom Hook for manging dispatch call for clearing message. */
+//   /* Signature => notificationMessage, dispatch, timeInSeconds */
+//   if (notification === null) {
+//     return null
+//   }
+
+//   const style = {
+//     border: "solid",
+//     padding: 10,
+//     borderWidth: 1
+//   };
+//   return <div style={style}>{notification.message}</div>
+// } /* End of Notification Component here. */
+
+// export default Notification
+
+// const useClearNotificationHook = (notificationMessage, dispatch, timeInSeconds) => {
+//   console.log = () => {} /* COMMENT this line to activate all the console.logs inside this hook. */
+//   let timeIdRef = React.useRef()
+//   React.useEffect(() => {
+//     if (notificationMessage) {
+//       timeIdRef.current = setTimeout(() => {
+//         dispatch(clearNotification())
+//         /* LOG1 */ console.log("::useeffect::DISPATCH successful for clearing notification.")
+//         timeIdRef.current = null /* This will prevent un-necessary execution of clearUp function. */
+//       }, timeInSeconds * 1000)
+//       /* LOG2 */ console.log("::useeffect::SCHEDULED async dispatch call for id => ", timeIdRef.current)
+//       return () => {
+//         if (timeIdRef.current) {
+//           clearTimeout(timeIdRef.current)
+//           /* LOG3 */ console.log("::cleanUpFunction:: CLEARED async dispatch call for id => ", timeIdRef.current)
+//         }
+//       }
+//     }
+//   }, [notificationMessage, dispatch, timeInSeconds])
+// }
--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,28 +1,69 @@
 const notificationReducer = (state = null, action) => {
-  switch (action.type) {
-    case "NOTIFY":
-      return action.data
-    case "CLEAR_NOTIFICATION":
-      return (state = null)
+    switch (action.type) {
+      case 'NOTIFY':
+        return action.data
+      case 'CLEAR_NOTIFICATION':
+        return state = null
 
-    default:
-      return state
+      default:
+        return state
+    }
   }
-}
 
-export const clearNotification = () => {
-  return {
-    type: "CLEAR_NOTIFICATION",
+  export const clearNotification = () => {
+      return {
+          type: 'CLEAR_NOTIFICATION'
+      }
   }
-}
 
-export const setNotification = (message, seconds) => {
-  return (dispatch) => {
-    dispatch({
-      type: "NOTIFY",
-      data: { message },
-    })
-  }
-}
+  let timeoutID
 
-export default notificationReducer
+  export const setNotification = (message, seconds) => {
+    console.log("timeoutID: ", timeoutID)
+  
+    return async (dispatch) => {   
+      dispatch({
+      type: 'NOTIFY',
+      data: {message},
+      })
+
+      if (timeoutID) {
+        clearTimeout(timeoutID)
+      }
+      
+      timeoutID = setTimeout(() => {
+      dispatch(clearNotification())
+      }, seconds * 1000)     
+    }
+ }
+
+  export default notificationReducer
+
+// const notificationReducer = (state = null, action) => {
+//   switch (action.type) {
+//     case "NOTIFY":
+//       return action.data
+//     case "CLEAR_NOTIFICATION":
+//       return (state = null)
+
+//     default:
+//       return state
+//   }
+// }
+
+// export const clearNotification = () => {
+//   return {
+//     type: "CLEAR_NOTIFICATION",
+//   }
+// }
+
+// export const setNotification = (message, seconds) => {
+//   return (dispatch) => {
+//     dispatch({
+//       type: "NOTIFY",
+//       data: { message },
+//     })
+//   }
+// }
+
+// export default notificationReducer

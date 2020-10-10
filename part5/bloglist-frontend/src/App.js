38d9240ca4900f963100284d73fd87f7659d5431
jonathandennis <4jondennis@gmail.com>
2020-10-10 21:34:40 +0200

    7.12: redux, step4 - Store information about the signed in user in the Redux store

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -8,30 +8,27 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 
 import { initializeBlogs } from './reducers/blogReducer'
+import { initializeUser, setUser, setUserNull } from './reducers/userReducer'
 import { setNotification } from './reducers/notificationReducer'
 import { useDispatch, useSelector } from 'react-redux'
 
 const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
+
+  const blogs = useSelector(state => state.blogs)
+  const user = useSelector(state => state.user)
 
   const dispatch = useDispatch()
-  const blogs = useSelector(state => state.blogs)
-
 
   useEffect(() => {
     dispatch(initializeBlogs())
   }, [dispatch])
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
-    }
-  }, [])
+    dispatch(initializeUser())
+  }, [dispatch])
+
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -46,7 +43,7 @@ const App = () => {
         'loggedBlogappUser', JSON.stringify(user)
       )
       blogService.setToken(user.token)
-      setUser(user)
+      dispatch(setUser())
       setUsername('')
       setPassword('')
     } catch (error) {
@@ -61,7 +58,7 @@ const App = () => {
     window.localStorage.removeItem(
       'loggedBlogappUser', JSON.stringify(user)
     )
-    setUser(null)
+    dispatch(setUserNull(null))
     dispatch(setNotification(`${user.name} has been sucessfully logged out.`, 5))
     //notify(`${user.name} has been sucessfully logged out.`, 'ok')
   }
@@ -102,6 +99,7 @@ const App = () => {
           key={blog.id}
           user={user}
           blog={blog}
+
         />
       )}
       <Footer />

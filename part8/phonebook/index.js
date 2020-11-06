822674fb45829f612ebd1ee7e11966964bca762c
jonathandennis <4jondennis@gmail.com>
2020-11-06 16:31:56 +0100

    Completed lesson 8a

--- part8/phonebook/index.js
+++ part8/phonebook/index.js
@@ -1,4 +1,5 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let persons = [
   {
@@ -27,24 +28,84 @@ const typeDefs = gql`
   type Person {
     name: String!
     phone: String
-    street: String!
-    city: String! 
+    address: Address!
     id: ID!
+  }
+
+  type Address {
+    street: String!
+    city: String!
   }
 
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
+
+    enum YesNo {
+        YES
+        NO
+    }
   }
 `
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+        if (!args.phone) {
+          return persons
+        }
+        const byPhone = (person) =>
+          args.phone === 'YES' ? person.phone : !person.phone
+        return persons.filter(byPhone)
+      },
     findPerson: (root, args) =>
       persons.find(p => p.name === args.name)
+  },
+  Person: {
+    address: (root) => {
+      return { 
+        street: root.street,
+        city: root.city
+      }
+    }
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find(p => p.name === args.name)) {
+          throw new UserInputError('Name must be unique', {
+            invalidArgs: args.name,
+          })
+      }
+
+      const person = { ...args, id: uuid() }
+      persons = persons.concat(person)
+      return person
+    },
+    editNumber: (root, args) => {
+        const person = persons.find(p => p.name === args.name)
+        if (!person) {
+          return null
+        }
+    
+        const updatedPerson = { ...person, phone: args.phone }
+        persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+        return updatedPerson
+      }   
   }
 }
 

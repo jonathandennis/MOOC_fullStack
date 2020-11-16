540c4bef1b0791ae5111221f00bc9604836b4680
jonathandennis <4jondennis@gmail.com>
2020-11-16 12:50:13 +0100

    8.8: Authors view - Implement an Authors view to show the details of all authors on a page

--- part8/library/client/src/App.js
+++ part8/library/client/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
+//import { ALL_AUTHORS } from './queries'
+
 const App = () => {
   const [page, setPage] = useState('authors')
+
+  //const result = useQuery(ALL_AUTHORS)
+
+  //console.log('result: ', result)
 
   return (
     <div>
@@ -16,6 +23,7 @@ const App = () => {
 
       <Authors
         show={page === 'authors'}
+        //authors={result.data.allAuthors}
       />
 
       <Books
--- part8/library/client/src/components/Authors.js
+++ part8/library/client/src/components/Authors.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useQuery } from '@apollo/client'
+
+import { ALL_AUTHORS } from '../queries'
 
 const Authors = (props) => {
+    console.log('props: ', props)
+    const result = useQuery(ALL_AUTHORS)
+    console.log('result: ', result)
   if (!props.show) {
     return null
   }
-  const authors = []
+  const authors = result.data.allAuthors
 
   return (
     <div>
